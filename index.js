182adeb5191af24eb833ea5068a35647e07c55fc
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-22 11:13:40 -0400

    cookie consent
    -add modal message with innerHTML

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
 const consentForm = document.getElementById('consent-form')
+const modalText = document.getElementById("modal-text")
 
 setTimeout(function(){
     modal.style.display = 'inline'
-}, 300)
+}, 1500)
 
 modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
@@ -12,5 +13,28 @@ modalCloseBtn.addEventListener('click', function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
-    console.log('form submitted')
+    modalText.innerHTML = `
+    <div class="modal-inner-loading">
+        <img src="images/loading.svg" class="loading">
+        <p id="uploadText">
+            Uploading your data to the dark web...
+        </p>
+    </div>
+`
+/*
+Challenge:
+1. Take control of the "modal-text" element. 
+2. Make it so that when a user clicks on 
+   the accept button, the HTML string below
+   is inserted into the modal-text div.
+
+<div class="modal-inner-loading">
+    <img src="images/loading.svg" class="loading">
+    <p id="uploadText">
+        Uploading your data to the dark web...
+    </p>
+</div>
+*/ 
 })
+ 
+
