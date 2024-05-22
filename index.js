ec1e09c7f82704cd230460b96d3e6cf2a09f7858
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-22 10:57:20 -0400

    cookie consent
       1. Take control of the form element. 
    2. Add an eventListener to the form to listen for a
       "submit" event.
    3. When a user clicks "accept", prevent the default
       behaviour that triggers the refresh.
    4. Log out "form submitted".

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
+const consentForm = document.getElementById("consent-form")
 
 setTimeout(function(){
     modal.style.display = 'inline'
@@ -9,3 +10,18 @@ modalCloseBtn.addEventListener('click', function(){
     modal.style.display = 'none'
 })
 
+/*
+Challenge:
+1. Take control of the form element. 
+2. Add an eventListener to the form to listen for a
+   "submit" event.
+3. When a user clicks "accept", prevent the default
+   behaviour that triggers the refresh.
+4. Log out "form submitted".
+*/  
+
+
+consentForm.addEventListener("submit", function(e){
+    e.preventDefault()
+    console.log("form submitted")
+})
