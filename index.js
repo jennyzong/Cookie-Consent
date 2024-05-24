f72e0c59475d2c8bdb89ed739f13e94f3fcc749b
jennyzong <134018092+jennyzong@users.noreply.github.com>
2024-05-23 21:22:40 -0400

    cookie consent
    -create a const and set it equals to a new 
       instance of FormData which takes in our HTML 
       form element as a parameter

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ modalCloseBtn.addEventListener('click', function(){
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
+
+/*   
+Challenge: 
+1. Inside the body of this event listener, 
+   create a const and set it equals to a new 
+   instance of FormData which takes in our HTML 
+   form element as a parameter.
+2. Log out the const (you should just get 
+   a FormData object).
+*/ 
+    
+    
     modalText.innerHTML = `
     <div class="modal-inner-loading">
         <img src="images/loading.svg" class="loading">
@@ -20,30 +32,19 @@ consentForm.addEventListener('submit', function(e){
     </div>` 
     
     setTimeout(function(){
-        document.getElementById('upload-text').innerText = "Making the sale..."
+        document.getElementById('upload-text').innerText = `
+        Making the sale...`
     }, 1500)
-   setTimeout(function(){
+    
+    
+    setTimeout(function(){
         document.getElementById('modal-inner').innerHTML = `
         <h2>Thanks you sucker! </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
-        <img src="images/pirate.gif">
+            <img src="images/pirate.gif">
         </div>
-       `
-   },3000)
-/*   
-Challenge: 
-1. Make it so that 1.5 seconds after seeing the 
-   "Making the sale..." message, the modal is 
-   cleared of its content and the following 
-   string of HTML is displayed instead.
-   
-   `<h2>Thanks you sucker! </h2>
-    <p>We just sold the rights to your eternal soul.</p>
-    <div class="idiot-gif">
-        <img src="images/pirate.gif">
-    </div>
-    ` 
-*/  
-
+    `
+    }, 3000)
+  
 }) 
